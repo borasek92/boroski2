3dd8b2668b2f5d0c440e9e1b604786bcee09dad6
borasek92 <BARTLOMIEJ.BOROSKI@GMAIL.COM>
2025-03-14 22:39:00 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,40 @@
+'use client';
+
 import './styles/globals.css';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Preloader from './components/Preloader';
 
-export const metadata = {
-  title: 'Bartłomiej Boroski | Kierownik Projektów IT',
-  description: 'Ekspert wdrożeń systemów ERP z 13-letnim doświadczeniem. Specjalista integracji, automatyzacji procesów i wykorzystania AI w biznesie.',
-  keywords: 'kierownik projektów IT, wdrożenia ERP, Navireo, Subiekt GT, automatyzacja procesów, integracja systemów, sztuczna inteligencja w biznesie',
-};
-
 export default function RootLayout({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    // Dodaj opóźnienie, aby symulować ładowanie strony
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Preloader będzie aktywny przez 3 sekundy
+    
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
     <html lang="pl" className="scroll-smooth">
+      <head>
+        <title>Bartłomiej Boroski | Kierownik Projektów IT</title>
+        <meta name="description" content="Ekspert wdrożeń systemów ERP z 13-letnim doświadczeniem. Specjalista integracji, automatyzacji procesów i wykorzystania AI w biznesie." />
+        <meta name="keywords" content="kierownik projektów IT, wdrożenia ERP, Navireo, Subiekt GT, automatyzacja procesów, integracja systemów, sztuczna inteligencja w biznesie" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Preloader />
-        <Navbar />
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
+        
+        <div className={isLoading ? 'content-loading' : 'content-loaded'}>
+          <Navbar />
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
